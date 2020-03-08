00f0957ca39afd644040e48cb81dff54908d5e1e
LukaszBrzoska <s16399@pjwstk.edu.pl>
2020-03-08 08:58:27 +0100

    BMI calculator

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.bmicalculator;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText height;
+    private EditText weight;
+    private TextView result;
+    private Button calculate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        height = findViewById(R.id.height);
+        weight = findViewById(R.id.weight);
+        result = findViewById(R.id.result);
+        calculate = findViewById(R.id.calculate);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateBMI();
+            }
+        });
+    }
+
+    public void calculateBMI() {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if(heightStr != null && !"".equals(heightStr)
+                && weightStr != null && !"".equals(weightStr)) {
+
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi = weightValue / (heightValue * heightValue);
+
+            displayBMI(bmi);
+    }
+}
+
+    private void displayBMI(float bmi) {
+        String bmiDisplay = "";
+
+        if(Float.compare(bmi, 15f) <= 0) {
+            bmiDisplay = getString(R.string.wyglodzenie);
+        } else if(Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 16f) <= 0){
+            bmiDisplay = getString(R.string.wychudzenie);
+        } else if(Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 18.5f) <= 0){
+            bmiDisplay = getString(R.string.niedowaga);
+        } else if(Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 25f) <= 0){
+            bmiDisplay = getString(R.string.pozadana_masa_ciala);
+        } else if(Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 30f) <= 0){
+            bmiDisplay = getString(R.string.nadwaga);
+        } else if(Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 35f) <= 0){
+            bmiDisplay = getString(R.string.otylosc_I_stopnia);
+        } else if(Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 40f) <= 0){
+            bmiDisplay = getString(R.string.otylosc_II_stopnia);
+        } else {
+            bmiDisplay = getString(R.string.otylosc_III_stopnia);
+        }
+
+        bmiDisplay = bmi + "\n\n" + bmiDisplay;
+        result.setText(bmiDisplay);
+    }
+}
